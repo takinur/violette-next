0349ea63e50eb9e117710f3a29ee66f7251c3fc7
Takinur M <takinurm@gmail.com>
2023-02-11 23:09:04 +0600

    Added Wraith Page

---
+++ src/pages/wraith.tsx
@@ -0,0 +1,12 @@
+//Another Page Goes Here
+import React from 'react';
+
+const Wraith = () => {
+    return (
+        <div>
+            <h1> Hi, Wraith</h1>
+        </div>
+    );
+};
+
+export default Wraith;
